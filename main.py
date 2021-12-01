0e483fdfe629055d2af5a3ed713a3c173b1e761f
Oleg Tarasov <tolegu@ya.ru>
2021-12-01 21:30:16 +0300

    добавлен логгинг

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ Telegram bot checking the lessons at DVMN site and reporting the result.
 """
 import requests
 import telegram
+import logging
 from textwrap import dedent
 from time import sleep
-from tqdm import trange
 from config import URL, HEADERS, TIMEOUT, TG_TOKEN, CHAT_ID, CONNECTION_RETRY_WAITING_TIME
 
 
@@ -18,7 +18,7 @@ is_negative_text = {
 def get_parsed_answer(attempts: list) -> str:
     answer = ''
     for attempt in attempts:
-        answer += f"""\
+        answer += f"""
         Проверена работа {attempt['lesson_title']}
         {is_negative_text[attempt['is_negative']]}
         Ссылка на задачу: {attempt['lesson_url']}
@@ -27,13 +27,18 @@ def get_parsed_answer(attempts: list) -> str:
     return dedent(answer)
 
 
+def send_tg_message(text: str, chat_id=CHAT_ID, token=TG_TOKEN):
+    """Send message to TG chat with chat_id using TG token."""
+    bot = telegram.Bot(token=token)
+    bot.send_message(chat_id=chat_id, text=text)
+    logger.info('Message sent.')
+
+
 def get_works():
-
+    """Get reviewed works at DVMN server."""
     request_params = {}
-    bot = telegram.Bot(token=TG_TOKEN)
-
+    
     while True:
-        print('Sending request ...')
         try:
             response = requests.get(URL,
                                     headers=HEADERS,
@@ -42,29 +47,43 @@ def get_works():
                                     )
             response.raise_for_status()
         except requests.ReadTimeout:
+            logger.info('Timeout.')
             continue
         except requests.ConnectionError:
-            print('No server connection.')
-            print(f'Waiting for {CONNECTION_RETRY_WAITING_TIME} seconds.')
-            for i in trange(CONNECTION_RETRY_WAITING_TIME):
+            logger.warning('No server connection.')
+            for i in range(CONNECTION_RETRY_WAITING_TIME):
                 sleep(1)            
             continue
 
         works = response.json()
         if works['status'] == 'timeout':
             request_params["timestamp"] = works["timestamp_to_request"]
-            print('no new works.')
             continue
 
         if works['status'] == 'found':
+            logger.info('Found reviewed works.')
             request_params["timestamp"] = works["last_attempt_timestamp"]
             answer = get_parsed_answer(works['new_attempts'])
-            bot.send_message(chat_id=CHAT_ID, text=answer)
+            send_tg_message(answer)
+
+
+def main():
+    """Application entry point."""
+    logger = logging.getLogger('main')
+    logger.setLevel(logging.INFO)
+    
+    fh = logging.FileHandler('main.log')
+    formatter = logging.Formatter('%(asctime)s - line %(lineno)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    
+    logger.addHandler(fh)
+
+    logger.info('Program started.')
+    get_works()
+    logger.info('Done!')
 
 
 if __name__ == '__main__':
 
-    try:
-        get_works()
-    except KeyboardInterrupt:
-        exit()
+    main()
+
