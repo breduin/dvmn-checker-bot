9d79cde208ed613df80a96e8036773920619586e
Oleg Tarasov <tolegu@ya.ru>
2021-12-04 20:48:51 +0300

    улучшения: удален config.py, высушен код, убрана обертка

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 """
 Telegram bot checking the lessons at DVMN site and reporting the result.
 """
+import os
 import sys
 import requests
 import telegram
@@ -8,8 +9,6 @@ import logging
 from logging.handlers import RotatingFileHandler
 from textwrap import dedent
 from time import sleep
-from config import URL, HEADERS, TIMEOUT, TG_TOKEN, CHAT_ID
-from config import CONNECTION_RETRY_WAITING_TIME, LogsOutput as LO
 
 
 logger = logging.getLogger(__file__)
@@ -32,14 +31,21 @@ def get_parsed_answer(attempts: list) -> str:
     return dedent(answer)
 
 
-def send_tg_message(text: str, chat_id=CHAT_ID, token=TG_TOKEN):
-    """Send message to TG chat with chat_id using TG token."""
-    bot = telegram.Bot(token=token)
-    bot.send_message(chat_id=chat_id, text=text)
-
-
 def get_works():
     """Listen DVMN server and get reviewed works as soon as they appear."""
+    # Время в сек., в течение которого бот ожидает ответ сервера dvmn.org
+    TIMEOUT = 90
+
+    # Пауза в сек. между попытками бота подключиться к серверу dvmn.org
+    CONNECTION_RETRY_WAITING_TIME = 60
+
+    # Заголовок запроса с ключом DVMN
+    HEADERS = {
+    'Authorization': f'Token {os.environ["DVMN_TOKEN"]}'
+    }
+    # URL для API DVMN
+    URL = 'https://dvmn.org/api/long_polling/'
+
     request_params = {}
 
     while True:
@@ -52,12 +58,10 @@ def get_works():
                                     )
             response.raise_for_status()
         except requests.ReadTimeout:
-            logger.info('Timeout. No new works.')
             continue
         except requests.ConnectionError:
             logger.warning('No server connection.')
-            for i in range(CONNECTION_RETRY_WAITING_TIME):
-                sleep(1)
+            sleep(CONNECTION_RETRY_WAITING_TIME)
             continue
 
         works = response.json()
@@ -69,22 +73,35 @@ def get_works():
             logger.info('Found reviewed works.')
             request_params["timestamp"] = works["last_attempt_timestamp"]
             answer = get_parsed_answer(works['new_attempts'])
-            send_tg_message(answer)
-            logger.info('Message sent.')
+            logger.info(answer)
 
 
 def main():
     """Application entry point."""
+
+    # Ключ API Telegram
+    TG_TOKEN = os.environ['TG_TOKEN']
+    # ID пользователя Telegram
+    CHAT_ID = os.environ['CHAT_ID']
+    # Записывать логи в файл?
+    FILE = (os.getenv('FILE', 'False') == 'True')
+    # Выводить логи в терминал?
+    STREAM = (os.getenv('STREAM', 'False') == 'True')
+    # Выводить логи в телеграм-чат?
+    BOT = (os.getenv('BOT', 'False') == 'True')
+
     class BotHandler(logging.Handler):
+
+        bot = telegram.Bot(token=TG_TOKEN)
 
         def emit(self, record):
             log_entry = self.format(record)
-            send_tg_message(log_entry)
+            self.bot.send_message(chat_id=CHAT_ID, text=log_entry)
 
     # Set logging level.
     logger.setLevel(logging.DEBUG)
 
-    if LO.FILE:
+    if FILE:
         file_handler = RotatingFileHandler(
             'main.log',
             maxBytes=100000,
@@ -97,14 +114,14 @@ def main():
         file_handler.setFormatter(file_handler_formatter)
         logger.addHandler(file_handler)
 
-    if LO.STREAM:
+    if STREAM:
         stream_handler = logging.StreamHandler(sys.stdout)
         stream_handler.setLevel(logging.DEBUG)
         stream_formatter = logging.Formatter('%(asctime)s - %(message)s')
         stream_handler.setFormatter(stream_formatter)
         logger.addHandler(stream_handler)
 
-    if LO.BOT:
+    if BOT:
         bot_handler = BotHandler()
         bot_handler.setLevel(logging.INFO)
         bot_formatter = logging.Formatter('%(message)s')
@@ -114,20 +131,18 @@ def main():
     logger.debug('Bot started.')
     logger.info('Program started.')
     
-    while True:
-        try:
-            get_works()
-        except ZeroDivisionError:
-            logger.exception('Деление на ноль.')
-            logger.info('Бот упал. Отжался. Работает дальше.')
+    get_works()
 
     logger.info('Program stopped.')
 
 
 if __name__ == '__main__':
+
     try:
         main()
     except KeyboardInterrupt:
         logger.info('Program stopped.')
         exit()
-
+    except Exception as e:
+        logger.exception(e)
+        raise e
